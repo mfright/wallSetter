9fd89db624420bc7bc015a2b68599944e222d61b
mfright <uj020288@yahoo.co.jp>
2015-03-15 01:56:20 +0900

    r000
    preview

---
+++ wallSetter/wallSetter/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace sampleWall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkAutoChange = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "wallpaper.pngを壁紙に設定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "このアプリケーションのフォルダに、wallpaper.pngとして壁紙を保存し、\r\nボタンを押してください。";
+            // 
+            // chkAutoChange
+            // 
+            this.chkAutoChange.AutoSize = true;
+            this.chkAutoChange.Location = new System.Drawing.Point(83, 100);
+            this.chkAutoChange.Name = "chkAutoChange";
+            this.chkAutoChange.Size = new System.Drawing.Size(211, 28);
+            this.chkAutoChange.TabIndex = 2;
+            this.chkAutoChange.Text = "次以降このアプリケーション起動時に、\r\n自動的に壁紙設定しアプリを終了する。";
+            this.chkAutoChange.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 140);
+            this.Controls.Add(this.chkAutoChange);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "壁紙設定君";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkAutoChange;
+    }
+}
+
---
+++ wallSetter/wallSetter/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Text;
+using System.IO;
+
+namespace sampleWall
+{
+    public partial class Form1 : Form
+    {
+        String myPath = System.Windows.Forms.Application.StartupPath;
+
+        const uint SPI_SETDESKWALLPAPER = 20;
+        const uint SPIF_UPDATEINIFILE = 1;
+        const uint SPIF_SENDWININICHANGE = 2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, StringBuilder pvParam, uint fWinIni);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setWall();
+        }
+
+        private void setWall()
+        {
+            StringBuilder sb = new StringBuilder(myPath + "\\wallpaper.png");
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, (uint)sb.Length, sb, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(myPath+"\\auto.txt"))
+            {
+                setWall();
+                Application.Exit();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (chkAutoChange.Checked)
+            {
+                Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+                StreamWriter writer =
+                  new StreamWriter(myPath + "\\auto.txt", true, sjisEnc);
+                writer.WriteLine("このファイルが存在すると、自動的に壁紙を設定して終了します。\r\n"+
+                "自動で壁紙を設定させたくないときは、このファイルを削除してください。");
+                writer.Close();
+            }
+        }
+    }
+}
